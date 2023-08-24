fcf459de5d4d9be9c14ee1c9a188d90589506553
Mikhail Pilugin <tonopod8@yandex.ru>
2023-08-24 16:02:52 +0300

    client

--- stats-server/src/main/java/ru/practicum/statserver/model/GlobalVariables.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.statserver.model;
-
-import java.time.format.DateTimeFormatter;
-
-public class GlobalVariables {
-    public static final DateTimeFormatter FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-}
